a699b3ae40a89c4d7ed3a44a3272509a432b7075
adewalet777 <oluwawunmi777@gmail.com>
2025-08-19 20:48:55 +0100

    toggle theme

---
+++ prdt.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const themeToggle = document.getElementById('theme-toggle');
+    const body = document.body;
+
+    themeToggle.addEventListener('click', () => {
+        body.classList.toggle('dark-theme');
+        body.classList.toggle('light-theme');
+    });
+});
